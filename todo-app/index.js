46f1f8aa6c90ec28ed7e86fb5c8a75b8f7ecc7ef
sawadyecma <snowslidez@yahoo.co.jp>
2025-03-31 21:23:26 +0900

    feat: ejs

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/tasks", (req, res) => {
+  res.render("./tasks/index.ejs", {});
 });
 
 app.listen(port, () => {
